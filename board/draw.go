72f0928254d11842b39f5d86cacd1bbf3e868174
Redmaner <jakevdputten@gmail.com>
2018-09-06 12:48:54 +0200

    board: Draw the board dynamicly

--- board/draw.go
+++ board/draw.go
@@ -2,21 +2,44 @@ package board
 
 import (
   "fmt"
+  "strconv"
 )
 
 // Function to show the board of tic tac toe
 // This function is universal and is called every round
 func Draw(gameData []string) {
+  boardSize, _ := strconv.Atoi(gameData[0])
+  maxCells := boardSize * boardSize
+
+  // Draw the header
   clearScreen()
-  fmt.Println("------------------")
+  fmt.Println("---------------------")
   fmt.Println("Tic Tac Go:", version)
-  fmt.Println("------------------")
+  fmt.Println("---------------------")
+  fmt.Print("\n  ")
+
+  // Draw the board dynamicly, based on the size of the board (boardSize)
+  for i := 1; i <= maxCells; i++ {
+    fmt.Print(gameData[i])
+    if i % boardSize == 0 && i != maxCells {
+      printBoardLine(boardSize)
+    } else if i % boardSize == 0 && i == maxCells {
+      fmt.Print("\n")
+    } else {
+      fmt.Print(" | ")
+    }
+  }
+
+  // Draw the bottom
   fmt.Println("")
-  fmt.Println("  ", gameData[1], "|", gameData[2], "|", gameData[3])
-  fmt.Println("  -----------")
-  fmt.Println("  ", gameData[4], "|", gameData[5], "|", gameData[6])
-  fmt.Println("  -----------")
-  fmt.Println("  ", gameData[7], "|", gameData[8], "|", gameData[9])
-  fmt.Println("")
   fmt.Println("------------------")
+}
+
+
+func printBoardLine(boardSize int) {
+  fmt.Print("\n -")
+  for c := 1; c <= boardSize; c++ {
+    fmt.Print("---")
+  }
+  fmt.Print("-\n  ")
 }
