53fcc386fb6c2e4255aac1a9f132fd299267c1a2
Redmaner <jakevdputten@gmail.com>
2018-09-06 13:28:49 +0200

    main: Add adjustable boardSize

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/redmaner/tictacgo/game"
+import (
+  "os"
+  "strconv"
+  "github.com/redmaner/tictacgo/game"
+)
 
 func main() {
-  game.Start(3)
+  args := os.Args
+  if len(args) > 1 {
+    boardSize, _ := strconv.Atoi(args[1])
+    if boardSize > 0 && boardSize <= 100 {
+      game.Start(boardSize)
+    }
+  } else {
+    game.Start(3)
+  }
 }
